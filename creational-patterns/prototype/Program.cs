cfbda7a93d4e3a1decc3d9674c6ff840b15857d6
bobligity <bobligity@gmail.com>
2022-08-25 20:37:34 -0400

    Pushing prototype and singleton

---
+++ creational-patterns/prototype/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace PrototypeMethod{
+    class Program{
+        static void Main(string[] args){
+            Enemy e1 = new Enemy();
+            e1.Hp = 100;
+            e1.Name = "Goblin_1";
+            e1.enemyId = new EnemyId(1);
+
+            Enemy e2 = e1.ShallowCopy();
+            Enemy e3 = e1.DeepCopy();
+
+            Console.WriteLine("Before Values:");
+            Console.WriteLine("e1");
+            DisplayValues(e1);
+            Console.WriteLine("e2");
+            DisplayValues(e2);
+            Console.WriteLine("e3");
+            DisplayValues(e3);
+
+            e1.Hp = 185;
+            e1.Name = "Orc_1";
+            e1.enemyId.Id = 2;
+            
+            Console.WriteLine("\nAfter Values:");
+            Console.WriteLine("e1");
+            DisplayValues(e1);
+            Console.WriteLine("e2");
+            DisplayValues(e2);
+            Console.WriteLine("e3");
+            DisplayValues(e3);
+        }
+
+        public static void DisplayValues(Enemy e){
+            Console.WriteLine("\tName: {0:s} \tHp: {1:d}", e.Name, e.Hp);
+            Console.WriteLine("\tId: " + e.enemyId.Id);
+        }
+    }
+
+    public class Enemy{
+        public int Hp;
+        public string Name;
+        public EnemyId enemyId;
+
+        public Enemy ShallowCopy(){
+            return (Enemy) this.MemberwiseClone();
+        }
+        public Enemy DeepCopy(){
+            Enemy clone = (Enemy) this.MemberwiseClone();
+            clone.enemyId = new EnemyId(enemyId.Id);
+            clone.Name = new string(Name);
+            return clone;
+        }
+
+    }
+
+    public class EnemyId{
+        public int Id;
+        public EnemyId(int id){
+            this.Id = id;
+        }
+    }
+}
---
+++ creational-patterns/singleton/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace FactoryMethod{
+    class Program{
+        static void Main(string[] args){
+            Singleton s1 = Singleton.GetInstance();
+            Singleton s2 = Singleton.GetInstance();
+
+            if(s1 == s2){
+                Console.WriteLine("Singleton works");
+            } else {
+                Console.WriteLine("Singleton failed");
+            }
+        }
+    }
+
+    public sealed class Singleton{
+        private static Singleton _instance;
+        private Singleton(){}
+
+        public static Singleton GetInstance(){
+            if(_instance == null){
+                _instance = new Singleton();
+            }
+            return _instance;
+        }
+
+        public void doSomething(){
+            Console.WriteLine("Something?");
+        }
+
+    }
+}
