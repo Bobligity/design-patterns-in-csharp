fd211fc751307d79ed9d700e1c5f371b9f76e0f0
Bobligity <bobligity@pop-os.localdomain>
2022-09-19 21:21:36 -0400

    added mediator pattern

--- behavioral-patterns/mediator/Program.cs
+++ behavioral-patterns/mediator/Program.cs
@@ -1,7 +1,85 @@
 ﻿namespace MediatorPattern{
     class Program{
         static void Main(string[] args){
-            Console.WriteLine("Making a mediator");
+            Component1 component1 = new Component1();
+            Component2 component2 = new Component2();
+
+            new ConcreteMediator(component1, component2);
+            
+            Console.WriteLine("Client triggers operation A");
+            component1.DoA();
+
+            Console.WriteLine("Client triggers operation D");
+            component2.DoD();
         }
     }
+
+    public interface IMediator{
+        void Notify(object sender, string ev);
+    }
+
+    class BaseComponent{
+        protected IMediator _mediator;
+
+        public BaseComponent(IMediator mediator = null){
+            this._mediator = mediator;
+        }
+
+        public void SetMediator(IMediator mediator){
+            this._mediator = mediator;
+        }
+    }
+
+    class Component1 : BaseComponent{
+        public void DoA(){
+            Console.WriteLine("Component1 does A.");
+            this._mediator.Notify(this, "A");
+        }
+
+        public void DoB(){
+            Console.WriteLine("Component1 does B.");
+            this._mediator.Notify(this, "B");
+        }
+    }
+
+    class Component2 : BaseComponent{
+        public void DoC(){
+            Console.WriteLine("Component2 does C.");
+            this._mediator.Notify(this, "C");
+        }
+
+        public void DoD(){
+            Console.WriteLine("Component2 does D.");
+            this._mediator.Notify(this, "D");
+        }
+    }
+
+
+
+    class ConcreteMediator : IMediator{
+        private Component1 _component1;
+        private Component2 _component2;
+
+        public ConcreteMediator(Component1 component1, Component2 component2){
+            this._component1 = component1;
+            this._component1.SetMediator(this);
+
+            this._component2 = component2;
+            this._component2.SetMediator(this);
+        }
+
+        public void Notify(object sender, string ev){
+            if(ev == "A"){
+                Console.WriteLine("Mediator reacts on A and triggers the following:");
+                this._component2.DoC();
+            }
+            if(ev == "D"){
+                Console.WriteLine("Mediator reacts on D and triggers the following:");
+                this._component1.DoB();
+                this._component2.DoC();
+            }
+        }
+    }
+
+
 }
