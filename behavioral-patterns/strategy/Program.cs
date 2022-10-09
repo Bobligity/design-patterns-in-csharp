a105151eee5474365c547bdce928aa98243d6546
Bobligity <bobligity@pop-os.localdomain>
2022-10-09 18:50:14 -0400

    adding state, strategy, and template patterns

---
+++ behavioral-patterns/strategy/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace StrategyPattern{
+    class Program{
+        static void Main(string[] args){
+            Context context = new Context();
+            
+            Console.WriteLine("Client: Strategy is set to normal sorting");
+            context.SetStrategy(new ConcreteStrategyA());
+            context.DoSomething();
+
+            Console.WriteLine("Client: Strategy is set to reverse sorting");
+            context.SetStrategy(new ConcreteStrategyB());
+            context.DoSomething();
+        }
+    }
+
+    class Context{
+        private IStrategy _strategy;
+        public Context(){}
+
+        public Context(IStrategy strategy){
+            this._strategy = strategy;
+        }
+        public void SetStrategy(IStrategy strategy){
+            this._strategy = strategy;
+        }
+
+        public void DoSomething(){
+            Console.WriteLine("Context: Sorting data");
+            List<string> result = this._strategy.DoAlgorithm(new List<string> {"a", "b", "c", "d", "e", "f", "g", "h"});
+
+            string resultStr = string.Empty;
+            foreach(string element in result as List<string>){
+                resultStr += element + ",";
+            }
+            Console.WriteLine(resultStr);
+        }
+    }
+
+    class ConcreteStrategyA: IStrategy{
+        public List<string> DoAlgorithm(List<string> data){
+            data.Sort();
+            return data;
+        }
+    }
+
+    class ConcreteStrategyB: IStrategy{
+        public List<string> DoAlgorithm(List<string> data){
+            data.Sort();
+            data.Reverse();
+            return data;
+        }
+    }
+
+    public interface IStrategy{
+        List<string> DoAlgorithm(List<string> data);
+    }
+}
---
+++ behavioral-patterns/template/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace TemplatePattern{
+    class Program{
+        static void Main(string[] args){
+            Console.WriteLine("Same Client code  can work with different subclasses");
+            Client.ClientCode(new ConcreteClass1());
+
+            Console.WriteLine("\nSame Client code can work with different subclasses");
+            Client.ClientCode(new ConcreteClass2());
+        }
+    }
+
+    abstract class AbstractClass{
+        public void TemplateMethod(){
+            this.BaseOperation1();
+            this.RequiredOperation1();
+            this.BaseOperation2();
+            this.Hook1();
+            this.RequiredOperation2();
+            this.BaseOperation3();
+            this.Hook2();
+        }
+
+        protected void BaseOperation1(){
+            Console.WriteLine("AC says: I am doing some work");
+        }
+
+        protected void BaseOperation2(){
+            Console.WriteLine("AC says: I am doing some more work");
+        }
+
+        protected void BaseOperation3(){
+            Console.WriteLine("AC says: I am doing the rest of the work");
+        }
+
+        protected abstract void RequiredOperation1();
+        protected abstract void RequiredOperation2();
+
+        protected virtual void Hook1(){}
+        protected virtual void Hook2(){}
+    }
+
+    class ConcreteClass1: AbstractClass{
+        protected override void RequiredOperation1()
+        {
+            Console.WriteLine("ConcreteClass1 says: I am doing Operation1");
+        }
+
+        protected override void RequiredOperation2()
+        {
+            Console.WriteLine("ConcreteClass1 says: I am doing Operation2");
+        }
+    }
+
+    class ConcreteClass2: AbstractClass{
+        protected override void RequiredOperation1()
+        {
+            Console.WriteLine("ConcreteClass2 says: I am doing Operation1");
+        }
+
+        protected override void RequiredOperation2()
+        {
+            Console.WriteLine("ConcreteClass2 says: I am doing Operation2");
+        }
+        
+        protected override void Hook1(){
+            Console.WriteLine("ConcreteClass2 says: I am doing Hook1");
+        }
+    }
+
+    class Client{
+        public static void ClientCode(AbstractClass abstractClass){
+            abstractClass.TemplateMethod();
+        }
+    }
+}
