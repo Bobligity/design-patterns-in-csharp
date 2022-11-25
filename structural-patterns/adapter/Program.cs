a6f15f0e5b4950e62fd3aba76b8343624f3f5ac2
Bobligity <bobligity@pop-os.localdomain>
2022-11-25 01:23:40 -0500

    Adding adapter, bridge, and composite patterns

---
+++ structural-patterns/adapter/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace AdapterPattern{
+    class Program{
+        static void Main(string[] args){
+            Adaptee adaptee = new Adaptee();
+            ITarget target = new Adapter(adaptee);
+
+            Console.WriteLine("Adaptee interface is incompatible with the client");
+            Console.WriteLine("But with the adapter, client can cal it's method");
+
+            Console.WriteLine(target.GetRequest());
+        }
+    }
+
+    public interface ITarget{
+        string GetRequest();
+    }
+
+    class Adaptee{
+        public string GetSpecificRequest(){
+            return "Specific Request.";
+        }
+    }
+
+    class Adapter: ITarget{
+        private readonly Adaptee _adaptee;
+        public Adapter(Adaptee adaptee){
+            this._adaptee = adaptee;
+        }
+
+        public string GetRequest(){
+            return $"This is '{this._adaptee.GetSpecificRequest()}'";
+        }
+    }
+}
---
+++ structural-patterns/bridge/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace BridgePattern{
+    class Program{
+        static void Main(string[] args){
+            Client client = new Client();
+
+            Abstraction abstraction = new Abstraction(new ConcreteImplementationA());
+            client.ClientCode(abstraction);
+
+            abstraction = new Abstraction(new ConcreteImplementationB());
+            client.ClientCode(abstraction);
+        }
+    }
+
+    class Abstraction{
+        protected IImplementation _implementation;
+
+        public Abstraction(IImplementation implementation){
+            this._implementation = implementation;
+        }
+
+        public virtual string Operation(){
+            return "Abstract: Base operation with :\n\t" + _implementation.OperationImplementation();
+        }
+    }
+
+    class ExtendedAbstraction : Abstraction{
+        public ExtendedAbstraction(IImplementation implementation): base(implementation){}
+
+        public override string Operation()
+        {
+            return "ExtendedAbstraction: Extended operation with :\n\t" + base._implementation.OperationImplementation();
+        }
+    }
+
+    public interface IImplementation{
+        string OperationImplementation();
+    }
+
+    class ConcreteImplementationA: IImplementation{
+        public string OperationImplementation(){
+            return "ConcreteImplementationA: The result in platform A";
+        }
+    }
+
+    class ConcreteImplementationB: IImplementation{
+        public string OperationImplementation(){
+            return "ConcreteImplementationB: The result in platform B";
+        }
+    }
+
+    class Client{
+        public void ClientCode(Abstraction abstraction){
+            Console.WriteLine(abstraction.Operation());
+        }
+    }
+}
